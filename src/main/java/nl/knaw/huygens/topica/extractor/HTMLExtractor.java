a8c8e1e6382dc4bc0a6ad9aec986dadecc947ab6
GertjanFi <gertjan.filarski@huygens.knaw.nl>
2013-03-25 21:06:58 +0100

    initial commit

---
+++ src/main/java/nl/knaw/huygens/topica/extractor/HTMLExtractor.java
@@ -0,0 +1,14 @@
+package nl.knaw.huygens.topica.extractor;
+
+import java.io.InputStream;
+
+public class HTMLExtractor {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
